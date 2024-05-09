af5defdeda49bab3a47e5fdea8ce11c8f5d556ca
Masoud Seraj <masoud.seraj.1991@gmail.com>
2024-05-09 23:44:34 +0330

    fixing responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 export default {
-  prefix: 'tw-',
+  prefix: "tw-",
   content: ["*.html"],
   theme: {
     extend: {
@@ -11,33 +11,35 @@ export default {
       },
 
       colors: {
-        primary: "#a50385",
+        primary: "rgb(123, 48, 123)",
         secondary: "#ecc94b",
         muted: "#d4d4d4",
-        'g2': {
-          light: '#d1d5db',
-          DEFAULT: '#6b7280',
-          dark: '#374151',
+        g2: {
+          light: "#d1d5db",
+          DEFAULT: "#6b7280",
+          dark: "#374151",
         },
       },
 
       fontFamily: {
-        iran: 'IRANSans'
+        iran: "IRANSans",
       },
 
       screens: {
-      'tablet': '640px',
-      // => @media (min-width: 640px) { ... }
+        tablet: "640px",
+        // => @media (min-width: 640px) { ... }
 
-      'laptop': '1024px',
-      // => @media (min-width: 1024px) { ... }
+        laptop: "1024px",
+        // => @media (min-width: 1024px) { ... }
 
-      'desktop': '1500px',
-      // => @media (min-width: 1280px) { ... }
+        desktop: "1500px",
+        // => @media (min-width: 1280px) { ... }
+      },
+
+      boxShadow: {
+        dot: "0 2px 4px 0 #ff5f5f",
+      },
     },
-    },
-
-
   },
   plugins: [],
 };
